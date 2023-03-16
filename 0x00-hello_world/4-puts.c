f61078f408738efb5e915f3680a20aa35030c33f
AbdelrahmanMagdy9 <educatingebs171@gmail.com>
2023-03-16 09:07:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - Entry point
- * Description: 'prints a string'
+
+/**
+ * prints a string
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
